ee8031b87731450b519aad0ced160d75107f3c99
Mike Stone <michael.stone@wipro.com>
2016-03-23 14:22:28 +0000

    Created UI component heirachy

--- app/index.js
+++ app/index.js
@@ -1,15 +1,53 @@
+var USER_DATA = {
+  name: 'Mike Stone',
+  username: 'stonempv',
+  image: 'http://www.designsforme.com.au/catalog/images/thumbs/3.jpg'
+}
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
-	render: function() {
+var ProfilePic = React.createClass({
+	render: function(imageUrl) {
 		return (
-			<div> Hello, {this.props.name} </div>
+      <img src={this.props.imageUrl} style={{height:100, width: 100}} />
 		)
 	}
 });
 
+
+var ProfileLink = React.createClass({
+  render: function() {
+    return (
+      <a href={'https://www.github.com/'+ this.props.username}>
+        {this.props.username}
+      </a>
+    )
+  }
+});
+
+var ProfileName = React.createClass({
+  render: function(){
+    return (
+      <div>{this.props.name}</div>
+    )
+  }
+});
+
+
+var Avatar = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <ProfilePic imageUrl={this.props.user.image} />
+        <ProfileName name={this.props.user.name} />
+        <ProfileLink username={this.props.user.username} />
+      </div>
+    )
+  }
+})
+
 ReactDOM.render(
-	<HelloWorld name="Mike"/>, 
+	<Avatar user={USER_DATA}/>, 
 	document.getElementById('app')
 );
