823b769122cc8b186065b7751a080c150c7c2ff3
Mike Stone <michael.stone@wipro.com>
2016-03-29 14:42:35 +0100

    Render the battle results

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,13 +1,30 @@
 var React = require('react');
 var PropTypes = React.PropTypes;
-
-function puke(obj) {
-  return <pre>{JSON.stringify(obj, 2, ' ')}</pre>
-}
+var styles = require('../styles');
+var UserDetailsWrapper = require('../components/UserDetailsWrapper');
+var UserDetails = require('../components/UserDetails');
+var Link = require('react-router').Link;
 
 function Results(props) {
+  var winningIndex = props.scores[0] > props.scores[1] ? 0 : 1;
+  var loserIndex = winningIndex === 0 ? 1 : 0;
   return (
-    <div> Results: {puke(props)} </div>
+    <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
+      <h1>Results</h1>
+      <div className='col-sm-8 col-sm-offset-2'>
+        <UserDetailsWrapper header='Winner'>
+          <UserDetails score={props.scores[winningIndex]} info={props.playersInfo[winningIndex]} />
+        </UserDetailsWrapper>
+        <UserDetailsWrapper header='Loser'>
+          <UserDetails score={props.scores[loserIndex]} info={props.playersInfo[loserIndex]} />
+        </UserDetailsWrapper>
+      </div>
+      <div className='col-sm-12' style={styles.space}>
+        <Link to='/playerOne'>
+          <button type='button' className='btn btn-lg btn-danger'>Start Over</button>
+        </Link>
+      </div>
+    </div>
   )
 }
 
